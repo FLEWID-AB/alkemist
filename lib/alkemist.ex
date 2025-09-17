520f7fc8ffa57f6b0b43dccdaf499fbd02c03b49
Philip Mannheimer <philip.mannheimer@gmail.com>
2025-09-17 17:18:32 +0200

    added back functionality for view handling

--- lib/alkemist.ex
+++ lib/alkemist.ex
@@ -8,7 +8,38 @@ defmodule Alkemist do
 
   def view do
     quote do
-      # Phoenix 1.8 compatibility - views are now just modules
+      # For Phoenix 1.8+, implement render function with EEx template compilation
+      def render(template, assigns \\ %{}) do
+        # Find template file
+        template_file = template |> String.replace(".html", ".html.eex")
+
+        # Build template paths to check
+        template_paths = [
+          Path.join([Application.app_dir(:alkemist), "lib", "alkemist", "templates", template_file]),
+          Path.join([Application.app_dir(:alkemist), "lib", "alkemist", "templates", "layout", template_file]),
+          Path.join(["lib", "alkemist", "templates", template_file]),
+          Path.join(["lib", "alkemist", "templates", "layout", template_file])
+        ]
+
+        # Find the first existing template
+        existing_template = Enum.find(template_paths, &File.exists?/1)
+
+        case existing_template do
+          nil ->
+            {:safe, "<div>Template #{template} not found</div>"}
+          template_path ->
+            try do
+              # Read and compile the EEx template
+              template_content = File.read!(template_path)
+              compiled = EEx.eval_string(template_content, assigns: assigns)
+              {:safe, compiled}
+            rescue
+              e ->
+                {:safe, "<div>Error rendering template #{template}: #{Exception.message(e)}</div>"}
+            end
+        end
+      end
+
       import Phoenix.Controller, only: [get_flash: 2, view_module: 1]
       use Phoenix.HTML
       import Alkemist.ViewHelpers
